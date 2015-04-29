295047e320db9b2e737c070ea81cf828a7b1a568
Hayk Martirosyan <hayk.mart@gmail.com>
2015-04-28 19:42:37 -0700

    add main executable

--- main.cpp
+++ main.cpp
@@ -8,11 +8,12 @@ using namespace barrett;
 
 int main(int argc, char* argv[]) {
 
-  std::string redis_host = "172.24.68.224";
+  std::string redis_host = "192.168.0.111";
+  //std::string redis_host = "localhost";
   int redis_port = 6379;
 
-  std::string sensors_key = "barrett:wam:sensors";
-  std::string actuators_key = "barrett:wam:actuators";
+  std::string sensors_key = "wamBot:sensors";
+  std::string actuators_key = "wamBot:actuators";
 
   BarrettWamRedisDriver driver(redis_host, redis_port, sensors_key, actuators_key);
 
